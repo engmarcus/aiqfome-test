8358efdd33936c87988a8be66c2fc20c78ce2fc1
Marcus Vinicius <marcus.santos@hndlabs.com.br>
2025-06-19 00:16:23 -0300

    [marcus] - remove code test

--- app/Http/Clients/FakeEstoreClientAsync.php
+++ app/Http/Clients/FakeEstoreClientAsync.php
@@ -12,43 +12,6 @@ class FakeEstoreClientAsync extends HttpAsyncClient
     public function __construct() {
         $this->config( config('fakeapi.baseUrl'));
     }
-    /*public function fetchProductAsync(int $productId): PromiseInterface
-    {
-        $cacheKey = "products.{$productId}";
-        $cached = cache()->get($cacheKey);
-
-        if ($cached) {
-            return Create::promiseFor([
-                'statusCode' => 200,
-                'data' => $cached,
-                'message' => null,
-            ]);
-        }
-
-        $endpoint = $this->buildProductEndpoint($productId);
-
-        return $this->guzzleClient->getAsync($endpoint)
-            ->then(function ($response) use ($cacheKey) {
-                $body = (string) $response->getBody();
-                $data = json_decode($body, true);
-
-                if (!empty($data)) {
-                    cache()->put($cacheKey, $data, now()->addMinutes(10));
-                }
-
-                return [
-                    'statusCode' => $response->getStatusCode(),
-                    'data' => $data,
-                    'message' => null,
-                ];
-            }, function ($exception) {
-                return [
-                    'statusCode' => 500,
-                    'data' => null,
-                    'message' => $exception->getMessage(),
-                ];
-            });
-    }*/
 
     public function fetchProductAsync(int $productId): PromiseInterface
     {
